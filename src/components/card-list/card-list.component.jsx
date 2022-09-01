c37a0b1e114ff1adaac0ab2c4124a81673b7e7dd
jgbpessoa <jgbpessoa@gmail.com>
2022-09-01 16:52:39 -0500

    Remaining Class Components to Functional Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,18 +2,28 @@ import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    console.log("render from CardList");
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </div>
+  );
+};
+
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+//     console.log("render from CardList");
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <Card monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
